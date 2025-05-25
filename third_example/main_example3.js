d443808515a0911bbd77ca87fbe7e525dfe2c147
kundup <kundup61@hotmail.com>
2025-05-25 12:13:39 -0700

    minor adjustments on codes

--- third_example/main_example3.js
+++ third_example/main_example3.js
@@ -4,46 +4,27 @@ console.log(`hello${x}`);
 const canvasel = document.getElementById("canvas");
 const context = canvasel.getContext("2d");
 
-const WIDTH = canvasel.width =  800;
-const HEIGHT = canvasel.height = 800;
+const WIDTH = canvasel.width =  600;
+const HEIGHT = canvasel.height = 600;
 
+let velocityX = 2;
+let velocityY = 1;
 let rectList = []
+const rectColor = ["brown", "green", "pink", "red", "orange"];
 
-const rectColor = ["black", "green", "pink", "red", "yellow"];
-
-function spawnRect(n){
+function spawnRect(n = 1){
     for (let i = 0; i < n; i++){
         let rect = {
-            x: Math.random() * 100 + 200,
-            y: Math.random() * 100 + 200,
-            width: Math.random() * 100,
-            height : Math.random() * 100,
+            width: 20,
+            height : 20,
+            x: Math.random() * WIDTH / 2,
+            y: Math.random() * (HEIGHT - 20),            
             color : rectColor[Math.floor(Math.random() * rectColor.length)]        
         }
         rectList.push(rect)
     }
 }
-spawnRect(100);
-drawScore();
-drawRect();
-
-function drawScore() {
-    const score = document.createElement("div");
-    score.innerHTML = "Toplam rect sayısı: " + rectList.length;
-    score.style.position = "absolute";
-    score.style.top = "-50px";
-    score.style.right = "-5px";
-    score.style.fontSize = "20px";
-    score.style.padding = "8px 12px";
-    score.style.backgroundColor = "#f0f0f0";
-    score.style.border = "1px solid #333";
-    score.style.borderRadius = "5px";
-
-    // Canvas kapsayıcısına ekle
-    const container = document.getElementById("canvas-container");
-    container.appendChild(score);
-}
-
+spawnRect(5);
 
 function drawRect (){
     rectList.forEach(({x, y, ...rest})=>{
@@ -51,3 +32,23 @@ function drawRect (){
         context.fillRect(x, y, rest.width, rest.height)
     })
 }
+
+function updateRect(){
+
+    let target_number = rectList.length;
+    rectList.forEach((rect)=>{
+        rect.x+= velocityX;        
+    })
+
+    rectList = rectList.filter(rect=> rect.x < WIDTH);
+    spawnRect(target_number - rectList.length);    
+}
+
+function animate (){
+    context.clearRect(0, 0, WIDTH, HEIGHT);
+    drawRect();
+    updateRect();
+    requestAnimationFrame(animate)
+}
+
+animate();
