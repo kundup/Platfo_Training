6b5af564574ce2d3f2458559fe12a17672d2ece2
kundup <kundup61@hotmail.com>
2025-05-26 20:08:58 -0700

    fixed the fire mechanism

--- third_example/player.js
+++ third_example/player.js
@@ -10,49 +10,42 @@ export class Player {
         this.bullet = new Bullet(this.x, this.y);
         this.addKeyboardControl();
         this.fire = false;
-        this.direction = null;
     }
-
     draw (ctx){
         ctx.fillStyle = "black";
         ctx.fillRect(this.x, this.y, this.size, this.size);
         this.bullet.drawBullet(ctx);       
     }
-
     update(){ 
         
         if (this.fire){
             this.bullet.x -= 2;
-            if (this.x < 0){
+            if (this.bullet.x < 0){
                 this.fire = false;
                 this.bullet.x = this.x;
+                this.bullet.y = this.y;
             }
-        }
-
-        if (this.direction){
-            this.y -= this.velY;
-        } else {
-            this.y += this.velY;            
-        }        
-        this.bullet.y = this.y;
+        }      
     }
-    
-
     addKeyboardControl (){
         window.addEventListener("keydown", event => {
-            event.preventDefault()
+            if (["ArrowUp", "ArrowDown", " "].includes(event.key)){
+                event.preventDefault()
+            }
+            
             if (event.key=== "ArrowUp"){
-                this.direction = true;
-                this.update();
+                this.y -= this.velY
+                if (!this.fire) this.bullet.y = this.y
             }
-
             if(event.key === "ArrowDown"){
-                this.direction = false;
-                this.update();
+                this.y += this.velY;
+                if (!this.fire) this.bullet.y = this.y
             }
+            if(event.key === " " && !this.fire){
+                this.fire = true;
+                this.bullet.y = this.y; 
+                this.bullet.x = this.x;
 
-            if(event.key === "f"){
-                this.fire = true; 
             }
         })
     }
