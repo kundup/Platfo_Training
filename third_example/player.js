53953e7fde235c754573709eb750545831ff8b03
kundup <kundup61@hotmail.com>
2025-06-15 22:07:09 -0700

    helicopter image added

--- third_example/player.js
+++ third_example/player.js
@@ -3,18 +3,21 @@ import { Bullet } from "./bullet.js";
 export class Player {
     constructor(width){
         this.width = width
-        this.x = this.width - 50;
+        this.x = this.width - 150;
         this.y = 100;
         this.velY = 15;
         this.size = 25;    
-        this.bulletList = [];        
+        this.bulletList = []; 
+        this.image = document.getElementById("player");       
     }
     draw (ctx){
-        ctx.fillStyle = "black";
-        ctx.fillRect(this.x, this.y, this.size, this.size);
-        this.bulletList.forEach((bullets)=>{
-            bullets.drawBullet(ctx)
-        })       
+        // ctx.fillStyle = "black";
+        // ctx.fillRect(this.x, this.y, this.size, this.size);
+        // this.bulletList.forEach((bullets)=>{
+        //     bullets.drawBullet(ctx)
+        // }) 
+        
+        ctx.drawImage(this.image, this.x, this.y, 100, 100);
     }
     update(){ 
         this.bulletList.forEach((bullet, index)=>{
